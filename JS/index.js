80e21f05629b699a9757d1d2faa27e2a8b596d28
Shivrajsinh Maharaul <shivrajmaharaul688@gmail.com>
2026-07-07 10:49:12 +0530

    Pushing code

--- JS/index.js
+++ JS/index.js
@@ -1,32 +1,41 @@
 function CheckOdd(x) {
     return x % 2 != 1;
 }
-function CheckEven(x){
+function CheckEven(x) {
     return x % 2 == 0;
 }
 function CheckAdmin(x) {
-    if(x == "Admin") return true
+    if (x == "Admin") return true
     else false
 }
 
 function canVote(x) {
-    if(x >= 18) return true;
+    if (x >= 18) return true;
     else return false;
 }
 
 function canDrive(x) {
-    if(x >= 21) return true;
+    if (x >= 21) return true;
     else return false;
 }
 const checkWeekDays = (x) => {
-    if (x ==1) return "Monday"
-    else if(x == 2) return "Tuesday"
-    else if(x == 3) return "Wednesday"
-    else if(x == 4) return "Thursday"
-    else if(x == 5) return "Friday"
-    else if(x == 6) return "Saturday"
-    else if(x == 7) return "Sunday"
+    if (x == 1) return "Monday"
+    else if (x == 2) return "Tuesday"
+    else if (x == 3) return "Wednesday"
+    else if (x == 4) return "Thursday"
+    else if (x == 5) return "Friday"
+    else if (x == 6) return "Saturday"
+    else if (x == 7) return "Sunday"
     else return "Invalid Input"
 }
 
 console.log(checkWeekDays(3))
+
+
+let ob = [1, 2, 3, 4]
+
+function test(obje) {
+    console.log(obje)
+}
+
+test(ob)
